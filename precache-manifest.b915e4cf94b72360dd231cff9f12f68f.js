9fc5f36d088585f71e5dc89cb2ff65d4010dbbd0
skd <saikrishna2deep@gmail.com>
2020-05-11 13:40:00 +0530

    Updates

---
+++ precache-manifest.b915e4cf94b72360dd231cff9f12f68f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bd47398aa7de65aadbb101b50ab6af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b095f22f57e9f0b0f8e5",
+    "url": "./static/css/2.46a6b073.chunk.css"
+  },
+  {
+    "revision": "3aec16733e6ee0b002d6",
+    "url": "./static/css/main.35c87c4b.chunk.css"
+  },
+  {
+    "revision": "b095f22f57e9f0b0f8e5",
+    "url": "./static/js/2.5fc2aed3.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "./static/js/2.5fc2aed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aec16733e6ee0b002d6",
+    "url": "./static/js/main.766484de.chunk.js"
+  },
+  {
+    "revision": "f585165c7f24d23ffb81",
+    "url": "./static/js/runtime-main.f886bc87.js"
+  }
+]);
